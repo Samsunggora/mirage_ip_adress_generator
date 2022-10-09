78d503a2df267db0fa8bfa130f78516adda84a16
Samsunggora <samsunggora@gmail.com>
2022-10-09 11:10:46 +0200

    added an entry to the file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io;
+use std::io::Write;
 use rand::prelude::*;
 
 fn main() {
@@ -8,7 +9,7 @@ fn main() {
     let mut n: u128 = 0;
 
     let _user_stack_size = io::stdin().read_line(&mut stack_size).unwrap();
-    let _file = File::create("ips.txt").expect("Error!");
+    let mut file = File::create("ips.txt").expect("Error!");
 
     let mut all_ips = Vec::with_capacity(stack_size.trim().parse().expect("Index entered was not a number"));
 
@@ -17,15 +18,18 @@ fn main() {
         all_ips.push(x);
         n += 1;
     }
-    println!("{:?}", all_ips);
+
+    for content in all_ips {
+        file.write_all(content.as_bytes()).expect("Write error!");
+    }
 }
 
 fn ip_generator() -> String {
-    let mut rng = rand::thread_rng();
+    let mut rng = thread_rng();
     let x: u8 = rng.gen_range(0..=255);
     let y: u8 = rng.gen_range(0..=255);
-    let z: u8 = rng.gen_range(0..255);
-    let k: u8 = rng.gen_range(0..255);
-    let ip = format!("{}.{}.{}.{}", x, y, z, k);
+    let z: u8 = rng.gen_range(0..=255);
+    let k: u8 = rng.gen_range(0..=255);
+    let ip = format!("'{}.{}.{}.{}' ", x, y, z, k);
     return ip;
 }
